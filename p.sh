aa78da0ce1312b80a03c920647764e37438f79be
codes-abdullah <codes.abdullah@gmail.com>
2024-08-03 04:38:43 +0300

    v2

--- p.sh
+++ p.sh
@@ -5,20 +5,25 @@ let date_line=9
 let date_line_diff=10
 let forecast_offset=4
 let forecast_line=$((date_line + forecast_offset))
+#noon = 2
 let target_timing_offset=2
+
  
 
 for i in {0..2}; do
-	v=`head -n ${date_line} file | tail -n 1` && v=${v#* } && v=${v#* } && v=${v% *} | arr=($v)	
+	v=`head -n ${date_line} file | tail -n 1` && v=${v#* } && v=${v#* } && v=${v% *} && IFS=' ' read -r -a arr <<< "$v"
 	year=`date +%Y`
 	day=${arr[1]}
 	month=${arr[2]}
-	if [[ `date +%y` != "${day}" ]]; then echo "no equals: ${month}"; continue; fi
+	if [[ `date +%d` == "${day}" ]]; then
+		forecast_temp="`head -n ${forecast_line} file | tail -n 1 | grep -ohP "\+\d+" | head -n $target_timing_offset | tail -n 1`"
+		break;
+	fi
 	
 	
 	let date_line=$((date_line + date_line_diff))
 	let forecast_line=$((date_line + forecast_offset))
-	echo "${date_line} && ${forecast_line}"
-	break;
 done
 
+actual_temp="10"
+echo "${year} - ${month} - ${day} - ${actual_temp} - ${forecast_temp}"
